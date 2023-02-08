6484e5a55dc6b97dc04b77e2e36df6e188c98ce4
Oliver Bonham-Carter <obonhamcarter@allegheny.edu>
2023-02-08 14:59:07 -0500

    init

---
+++ src/code.r
@@ -0,0 +1,48 @@
+# Add your name here
+
+rm(list = ls()) # clear out the variables from memory to make a clean execution of the code.
+
+# If you want to remove all previous plots and clear the console, run the following two lines.
+graphics.off() # clear out all plots from previous work.
+
+cat("\014") # clear the console
+
+#########################################
+
+library(tidyverse)
+
+# We are using the iris dataset for the below activity
+
+# Q1. What is the the command to view the "iris" dataset as a table?
+
+# TODO
+
+
+
+# Q2. Describe the data dataset. Which columns could be plotted? Please explain how you know this.
+
+# TODO
+
+
+
+# Q4. Fix the below function for plotting Sepal.Length by Sepal.Width.
+
+# ggplot(data = iris) + geom_point( (x=Sepal.Length, y=Sepal.Width))
+
+# TODO
+
+
+
+# Q5. Modify the above command to include color in some way to provide more information about the dataset. Note: this will increase the number of dimensions of your plot. 
+
+# TODO 
+
+
+
+# Q6. In your new plot from Q5, explain what you see and provide some conclusion about a trend that you note.
+
+# TODO
+
+
+
+# Please be sure to add your name at the top.
